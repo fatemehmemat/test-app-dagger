4799a13b75059d3c36f217d0dac6680cb8121c0b
memar <memar@palphone.com>
2022-06-08 18:34:48 +0430

    -add buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 repositories {
     gradlePluginPortal()
     google()
@@ -10,10 +12,18 @@ plugins {
     `kotlin-dsl-precompiled-script-plugins`
 }
 
-//add classpath
+afterEvaluate {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            apiVersion = "1.5"
+            languageVersion = "1.5"
+        }
+    }
+}
+
 dependencies {
     implementation("com.android.tools.build:gradle:7.2.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.6.1")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.6.1")
 }
